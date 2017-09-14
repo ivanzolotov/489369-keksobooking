db1343bb550a1987aaa95b7cb082ea5e440173e7
ivanzolotov <ivanzolotov@ivanzolotov.ru>
2017-09-15 00:56:55 +0500

    Добавил универсальную debounce-функцию

---
+++ js/debounce.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function () {
+  var DEBOUNCE_INTERVAL = 300;
+  var lastTimeout;
+  window.debounce = function (fn) {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(fn, DEBOUNCE_INTERVAL);
+  };
+})();

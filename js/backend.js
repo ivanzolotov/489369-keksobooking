d6da9784d12f829ffdcfe7a362ffc8c68d84e5ce
ivanzolotov <ivanzolotov@ivanzolotov.ru>
2017-09-14 12:47:22 +0500

    Добавил библиотеку для AJAX-запросов

---
+++ js/backend.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function () {
+
+  var SERVER_URL = 'https://1510.dump.academy/keksobooking';
+
+  function setup(onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = 10000;
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError(xhr.response);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout / 1000 + ' секунд');
+    });
+
+    return xhr;
+  }
+
+  window.backend = {
+    load: function (onLoad, onError) {
+      var xhr = setup(onLoad, onError);
+      xhr.open('GET', SERVER_URL + '/data');
+      xhr.send();
+    },
+    save: function (data, onLoad, onError) {
+      var xhr = setup(onLoad, onError);
+      xhr.open('POST', SERVER_URL);
+      xhr.send(data);
+    },
+  };
+
+})();
